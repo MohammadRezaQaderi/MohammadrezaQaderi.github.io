9182a0e641ae5bb0b5d0f1c75e4c788ae8f0edaf
mohammadrezaqaderi <mrq112775@outlook.com>
2023-02-01 23:30:19 +0330

    add: data for static website is need for show infos

---
+++ src/data/educationData.js
@@ -0,0 +1,16 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Shahid Beheshti High School Sampad',
+        course: 'Higher Secondary Education',
+        startYear: '2014',
+        endYear: '2018'
+    },
+    {
+        id: 2,
+        institution: 'Amirkabir University of Technology',
+        course: 'Bachelor of Technology',
+        startYear: '2018',
+        endYear: '2022'
+    }
+]
---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Mohammadreza Qaderi',
+    title: "Web Developer",
+    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    image: 'https://avatars.githubusercontent.com/u/66215461?s=400&u=5ac4686f76af9ab3464fb9fcacdff4cd926fbd0a&v=4',
+    resumePdf: resume
+}
