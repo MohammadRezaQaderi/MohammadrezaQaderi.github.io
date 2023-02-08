b8266666e9693778aa50aa5be56beba3fbfedd37
mohammadrezaqaderi <mrq112775@outlook.com>
2023-02-08 11:15:26 +0330

    last release

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -19,6 +19,15 @@ export const projectsData = [
         image: one
     },
     {
+        id: 1,
+        projectName: 'Employee Management',
+        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
+        tags: ['React', 'CSS', 'Material Ui'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: one
+    },
+    {
         id: 2,
         projectName: 'Railway Tracking',
         projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
